69d9ef475355cf9d2d818b7b4b6879172d47fc77
NPNG1231 <88768592+NPNG1231@users.noreply.github.com>
2021-08-29 17:35:11 +0800

    1

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import json
+
+with open('setting.json','r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='[',intents = intents)
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
+    await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(int(jdata['Leave_channel']))
+    await channel.send(f'{member} leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+bot.run(jdata['TOKEN'])
